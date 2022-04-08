6bf9239f70b887b1393e57f4218d072bbd4111e1
lamnt95 <tunglam20132208@gmail.com>
2022-04-08 13:15:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-simple-hqn6en
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/express-simple-hqn6en)
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.send('cache');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
